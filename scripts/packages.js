04c894434012d909c3fcbebbdf119ab743498156
share <85334444+yoyoweb1@users.noreply.github.com>
2023-04-11 14:33:57 +0800

    Update packages.js

--- scripts/packages.js
+++ scripts/packages.js
@@ -41,10 +41,10 @@ module.exports = [
     name: 'luci-app-aria2',
     desc: 'aria2下载工具',
   },
-  {
-    name: 'luci-app-zerotier',
-    desc: 'ZeroTier内网穿透',
-  },
+ // {
+ //   name: 'luci-app-zerotier',
+ //   desc: 'ZeroTier内网穿透',
+ // },
   {
     name: 'luci-app-turboacc',
     desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
